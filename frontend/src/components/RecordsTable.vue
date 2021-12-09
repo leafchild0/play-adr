128a4af7b4ea94333316c5432d2d0c63bf251d5d
viktor.malyshev <viktor.malyshev-cs@insticator.com>
2021-12-09 23:08:05 +0200

    added table component and mocked data for it

---
+++ frontend/src/components/RecordsTable.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="records-table">
+    <b-table :data="data" :loading="loading"
+             :sort-icon="'arrow-up'"
+             :sort-icon-size="'is-small'"
+             :default-sort-direction="'asc'"
+             :default-sort="'status'"
+             striped
+             hoverable>
+
+      <b-table-column field="id" label="ID" width="40" numeric v-slot="props">
+        {{ props.row.id }}
+      </b-table-column>
+
+      <b-table-column field="name" label="Name" searchable centered v-slot="props">
+        {{ props.row.name }}
+      </b-table-column>
+
+      <b-table-column field="status" label="Status" searchable centered sortable width="200" v-slot="props">
+        {{ props.row.status }}
+      </b-table-column>
+
+      <b-table-column field="date" label="Date" centered v-slot="props">
+        {{ props.row.date }}
+      </b-table-column>
+
+      <b-table-column custom-key="actions" label="Actions" v-slot="props">
+        <b-button class="button is-small is-light"
+                  icon-left="circle-edit-outline"
+                  type="is-success is-light"
+                  rounded
+                  @click="edit(props.row)">
+        </b-button>
+      </b-table-column>
+    </b-table>
+  </div>
+</template>
+
+<script>
+// Table
+export default {
+  name: "RecordsTable",
+  props: ["data", "loading"],
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    edit: (row) => {
+      console.log(row);
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+  .records-table {
+    display: flex;
+    justify-content: center;
+  }
+
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,15 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <b-field label="Name">
-      <b-input value="Kevin Garvey"></b-input>
-    </b-field>
+    <RecordsTable :data="data" :loading="false"></RecordsTable>
   </div>
 </template>
 
 <script>
+import RecordsTable from "@/components/RecordsTable";
 export default {
   name: "Home",
-  components: {},
+  components: {RecordsTable},
+  data() {
+    return {
+      data: [
+        { 'id': 1, 'name': 'Learn Play Framework', 'status': 'SUGGESTED', 'date': '2016-10-15 13:43:27'},
+        { 'id': 2, 'name': 'Create this app', 'status': 'APPROVED', 'date': '2016-12-15 06:00:53'},
+        { 'id': 3, 'name': 'Use Gradle instead of sbt', 'status': 'SUGGESTED', 'date': '2016-04-26 06:26:28'},
+        { 'id': 4, 'name': 'Implement Mongo schema', 'status': 'APPROVED', 'date': '2016-04-10 10:28:46'},
+        { 'id': 5, 'name': 'Add Spring to the project', 'status': 'SUSPENDED', 'date': '2016-12-06 14:38:38'}
+      ],
+    }
+  }
 };
 </script>

9c2bef2666b4441c1e98ba8e6868113d1a8d0156
viktor.malyshev <viktor.malyshev-cs@insticator.com>
2021-12-09 17:38:56 +0200

    basic source for start with routing

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <b-field label="Name">
+      <b-input value="Kevin Garvey"></b-input>
+    </b-field>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
